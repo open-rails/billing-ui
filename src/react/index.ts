d2e2cbaaa95dd9b2a50f74ebd2f8b199c5423e13
Paul Fidika <paul@fidika.com>
2026-09-23 12:22:49 -0600

    Add account billing: client, React hooks and styled panels (WIP)

---
+++ src/react/index.ts
@@ -0,0 +1,21 @@
+// Headless React layer: provider plus state hooks. No styling or strings.
+export { BillingProvider, type BillingProviderProps } from "./provider"
+export {
+  useBillingClient,
+  type BillingChange,
+  type BillingContextValue,
+} from "./context"
+export {
+  useCurrencies,
+  usePaymentMethods,
+  usePayments,
+  useSubscriptions,
+  type ActionResult,
+  type PaymentMethodAction,
+  type PaymentMethodsState,
+  type PaymentsOptions,
+  type PaymentsState,
+  type SubscriptionAction,
+  type SubscriptionsOptions,
+  type SubscriptionsState,
+} from "./hooks"
